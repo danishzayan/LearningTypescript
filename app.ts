74960773311a05db124544a83d062de795c87fa9
Danish Zayan <danishzayan6@gmail.com>
2024-02-27 00:38:15 +0530

    Learning TypeScript

---
+++ app.ts
@@ -0,0 +1,23 @@
+// let num1 = 1;
+// function calculate(num1: number, num2: number) {
+//     return num1+num2;
+// }
+// console.log(calculate(num1,4));  
+
+
+// function getTotal(numbers: Array<number>) {
+//     return numbers.reduce((acc, item) => {
+//         return acc + item;
+//     }, 0);
+// }
+// console.log(getTotal([3,2,1]));
+
+
+// const user = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     role: 'professor',
+// };
+// console.log(user);
+
+
